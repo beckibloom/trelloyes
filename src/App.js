c4f404b86ee4e8e83f6eed5e9c2d6f21e8822bd4
Becki Blumer <rblumer13@gmail.com>
2019-06-28 08:09:24 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import List from './List.js';
+import STORE from './store.js';
 import './App.css';
 
 function App() {
+  const lists = STORE.lists.map((list) => {
+    for (let i = 0; i < STORE.lists.length; i++) {
+      let cards = STORE.lists[i].cardIds;
+      let cardArray = [];
+      for (let j = 0; j < cards.length; j++) {
+        let cardId = cards[j];
+        let oneCard = STORE.allCards[cardId];
+        cardArray.push(oneCard);
+      }
+    return (
+      <List key={list.id} header={list.header} cards={cardArray} />
+      )
+    }
+  })
+
   return (
-    <div className="App">
+    <main className='App'>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Trelloyes!</h1>
       </header>
-    </div>
+      <div className="App-list">
+        {lists}
+      </div>
+    </main>
   );
 }
 
---
+++ src/List.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from './Card.js';
+import './List.css';
+
+
+function List(props) {
+    const cardList = props.cards.map((card) => 
+        <Card key={card.id} title={card.title} content={card.content} />
+    )
+
+    return (
+        <section className='List'>
+            <header className='List-header'>
+                <h2>{props.header}</h2>
+            </header>
+            <div className='List-cards'>
+                {cardList}
+                <button type="button" className="List-add-button">
+                + Add Random Card
+                </button>
+            </div>
+        </section>
+    )
+}
+
+export default List;
